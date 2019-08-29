727b837586e6894ea7821dc21b349a25b05ced19
wangchenglong <luckymrwang@163.com>
2019-08-29 17:00:48 +0800

    运行初版

--- src/server/common/config.js
+++ src/server/common/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 var listenPort = 8091;
 var uploadFolder = path.join(__dirname, '../uploads');
-var zookeeperConfig = '192.168.2.80:2181,192.168.2.81:2181,192.168.2.82:2181';
+var zookeeperConfig = '127.0.0.1:2181';
 module.exports = {
     listenPort,
     uploadFolder,
